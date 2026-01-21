8d457f9a2b0b87e66c13a5f63e77cfda9b15b9a4
camelranchentertainment <camelranchentertainment@gmail.com>
2026-01-21 00:15:46 -0600

    Update dashboard.tsx

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,8 +1,1 @@
-export default function Dashboard() {
-  return (
-    <div style={{ padding: '2rem', textAlign: 'center' }}>
-      <h1>🎉 Dashboard Works!</h1>
-      <p>You successfully logged in!</p>
-    </div>
-  );
-}
+
