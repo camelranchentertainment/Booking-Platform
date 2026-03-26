99a50f020842354cdaa2b91929485926d1045292
camelranchentertainment <camelranchentertainment@gmail.com>
2026-03-26 17:24:06 -0500

    Add isAdmin property to user data

--- pages/index.tsx
+++ pages/index.tsx
@@ -72,6 +72,7 @@ export default function LandingPage() {
         email:    data.email,
         bandName: data.bandName,
         tier:     data.subscriptionTier,
+        isAdmin:  data.isAdmin || false,
         token:    data.accessToken,
       }));
       router.push('/dashboard');
